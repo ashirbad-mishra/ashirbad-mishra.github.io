e9704d4a9fd473d29beaa34587e0f964a4248711
ashirbad-mishra <ashirbadmishra10@gmail.com>
2026-08-29 17:19:32 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('nav a').forEach(a=>{if(a.getAttribute('href')===location.pathname.split('/').pop()||(location.pathname.endsWith('/')&&a.getAttribute('href')==='index.html'))a.classList.add('active')});
